b1f27c402de8818990b4601fa9c865879c4a40a7
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2024-01-21 10:01:39 +0900

    analyze the stagging

---
+++ 01-src_分析脚手架/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="">
+    <FirmInfo></FirmInfo>
+    <StaffInfo></StaffInfo>
+  </div>
+</template>
+
+<script>
+    // 引入组件
+    import FirmInfo from './components/FirmInfo.vue'
+    import StaffInfo from './components/StaffInfo.vue'
+
+    export default {
+        name:'App',
+        components:{
+            FirmInfo,
+            StaffInfo
+        }
+    }
+</script>
+
