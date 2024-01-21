8ea01cf2ece61894a73583273ca395ebaa85a729
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2024-01-21 12:45:49 +0900

    about ref property

---
+++ 02-src_ref属性/components/FirmInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="firm">
+    <h2>公司名称:{{name}}</h2>
+    <h2>公司地址:{{address}}</h2>
+  </div>
+</template>
+
+<script>
+    export default {
+        name:'FirmInfo',
+        data() {
+            return {
+                name:'729声工场',
+                address:'北京市朝阳区'
+            }
+        },
+    }
+</script>
+
+<style>
+    .firm{
+        background-color: #bfa;
+    }
+</style>
