5b336ee1e939ec1c064a969dbd371a0a32737864
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2024-01-24 22:53:05 +0900

    TodoList -- check & cancel

---
+++ 08-src_TodoList勾选和取消/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="todo-main">
+    <TodoItem 
+      v-for="todoObj in todos" 
+      :key="todoObj.id" 
+      :todo="todoObj" 
+      :checkTodo="checkTodo"
+    />
+  </ul>
+</template>
+
+<script>
+  import TodoItem from './TodoItem'
+  export default {
+      name:'TodoList',
+      components:{TodoItem},
+      // checkTodo只是在这里走一趟，立马要去TodoItem组件
+      props:[
+        'todos',
+        'checkTodo'
+      ]
+  }
+</script>
+
+<style scoped>
+  /*main*/
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
