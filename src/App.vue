6546078ffdba73d5ff4e19ee835c0fd2773761d0
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2024-01-27 13:12:56 +0900

    bind custom events and trigger event listeners

--- src/App.vue
+++ src/App.vue
@@ -1,123 +1,55 @@
 <template>
-  <div id="root">
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <HeaderAdd :addTodo="addTodo"/>
-        <TodoList :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo"/>
-        <FooterCount :todos="todos" :checkAllTodo="checkAllTodo" :clearAllTodo="clearAllTodo"/>
-      </div>
-    </div>
+  <div class="app">
+    <h1>{{msg}}</h1>
+    <!-- 通过父组件给子组件传递函数类型的props实现：子组件给父组件传递数据 -->
+    <FirmInfo :getFirmName="getFirmName"/><hr>
+
+    <!-- 方法1：由于v-once在StaffInfo组件标签上，所以是给StaffInfo组件实例对象身上绑定了事件，事件名叫atstaff，若触发了这个事件，getStaffName函数就会被调用 -->
+    <!-- 通过父组件给子组件绑定一个自定义事件实现：子组件给父组件传递数据 -->
+    <StaffInfo @atstaff="getStaffName"/>
+    <!-- 设置只触发一次 -->
+    <!-- <StaffInfo @atstaff.once="getStaffName"/> -->
+
+    <!-- 方法2：在mounted钩子中，使用ref给StaffInfo组件实例对象的staff绑定on API，给on API传参传递数据的函数，以备触发事件时调用 -->
+    <!-- 通过父组件给子组件绑定一个自定义事件实现：子组件给父组件传递数据 -->
+    <!-- <StaffInfo ref="staff"/> -->
   </div>
 </template>
 
 <script>
-  import HeaderAdd from './components/HeaderAdd'
-  import TodoList from './components/TodoList'
-  import FooterCount from './components/FooterCount'
+    import FirmInfo from './components/FirmInfo.vue'
+    import StaffInfo from './components/StaffInfo.vue'
     
-  export default {
-      name:'App',
-      components:{
-        HeaderAdd,
-        TodoList,
-        FooterCount
-      },
-      data(){
-        return {
-          // 不把todos写成空数组写死，在这里获取存储在localStorage的数据
-          // 若是第一次登陆或清空本地存储后刷新会报错，因为从本地存储获取的是null，所以加个[]，表示本地存储有数据就获取数据，没有就默认是空数组
-          todos:JSON.parse(localStorage.getItem('todos'))  || []
-        }
-      },
-      methods:{
-        // 添加todoItem的函数
-        addTodo(data){
-          // 向list头部添加新数据
-          this.todos.unshift(data)
-        },
-        // 勾选or取消勾选一个todoItem
-        checkTodo(id){
-          // 遍历todos
-          this.todos.forEach((todo)=>{
-            // 若遍历后的todo的id等于点击的item的id，则done值取反后赋值返回
-            if(todo.id===id) todo.done = !todo.done
-          })
-        },
-        // 删除一个todo
-        deleteTodo(id){
-          // 过滤，其实也是遍历一下，filter不改变原数组，所以把返回值赋值给this.todos
-          this.todos=this.todos.filter(todo=>{
-            // 将没有点击的剩下的todo返回
-            return todo.id !== id
-          })
-        },
-        // 全选or取消全选
-        checkAllTodo(done){
-          this.todos.forEach(todo=>{
-            todo.done=done
-          })
-        },
-        // 清除所有已完成的todo
-        clearAllTodo(){
-          this.todos=this.todos.filter(todo=>{
-            // 过滤掉done值为false的todo对象，这是为了去掉done值为true的todo对象
-            return !todo.done
-          })
-        }
-      },
-      watch:{
-        todos:{
-          // 深度监测
-          depp:true,
-          handler(value){
-            localStorage.setItem('todos',JSON.stringify(value))
+    export default {
+        name:'App',
+        components:{FirmInfo,StaffInfo},
+        data(){
+          return {
+            msg:'你好'
           }
+        },
+        methods:{
+          // 传统绑定事件监听方式传递数据
+          getFirmName(name){
+            console.log('App got firm name:',name);
+          },
+          // 自定义事件方式绑定事件监听，传递数据
+          getStaffName(name,...rest){
+            console.log('App got staff name:',name,...rest);
+          }
+        },
+        mounted(){
+          // 绑定自定义事件，正常触发调用
+          // this.$refs.staff.$on('atstaff',this.getStaffName)
+          // 绑定自定义事件，一次性(和.once一样，设置触发一次就不会触发第二次)
+          // this.$refs.staff.$once('atstaff',this.getStaffName)
         }
-      }
-  }
+    }
 </script>
 
-<style>
-  /*base*/
-  body {
-    background: #fff;
-  }
-
-  .btn {
-    display: inline-block;
-    padding: 4px 12px;
-    margin-bottom: 0;
-    font-size: 14px;
-    line-height: 20px;
-    text-align: center;
-    vertical-align: middle;
-    cursor: pointer;
-    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-    border-radius: 4px;
-  }
-
-  .btn-danger {
-    color: #fff;
-    background-color: #da4f49;
-    border: 1px solid #bd362f;
-  }
-
-  .btn-danger:hover {
-    color: #fff;
-    background-color: #bd362f;
-  }
-
-  .btn:focus {
-    outline: none;
-  }
-
-  .todo-container {
-    width: 600px;
-    margin: 0 auto;
-  }
-  .todo-container .todo-wrap {
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
+<style scoped>
+  .app{
+    background-color: lightpink;
+    padding: 5px;
   }
 </style>
---
+++ src/components/StaffInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="staff">
+    <h2>员工姓名:{{name}}</h2>
+    <h2>员工性别:{{gender}}</h2>
+    <button @click="sendStaffName">click to send staff name to App</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name:'StaffInfo',
+        data() {
+            return {
+                name:'谷江山',
+                gender:'male'
+            }
+        },
+        methods:{
+            sendStaffName(){
+                // 触发StaffInfo组件实例对象身上的atstaff事件
+                this.$emit('atstaff',this.name,666,111,'hhh')
+            }
+        }
+    }
+</script>
+<style scoped>
+    .staff{
+        background-color: #bfa;
+        padding: 5px;
+        margin-top: 30px;
+    }
+</style>
