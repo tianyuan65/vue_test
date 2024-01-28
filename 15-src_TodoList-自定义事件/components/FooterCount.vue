e2c5a8883f46c200d779756d32dc6ed6d9d49414
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2024-01-28 19:44:42 +0900

    TodoList case with custom events

---
+++ 15-src_TodoList-自定义事件/components/FooterCount.vue
@@ -0,0 +1,83 @@
+<template>
+  <!-- 配置v-show属性，若total值等于0时隐藏底部，但凡有一个事项则展示底部 -->
+  <div class="todo-footer" v-show="total">
+    <label>
+      <!-- 各自创建函数法 -->
+      <!-- <input type="checkbox" :checked="isAll" @change="checkAll"/> -->
+      <!-- 双向数据绑定法 -->
+      <input type="checkbox" v-model="isAll"/>
+    </label>
+    <span>
+      <span>已完成{{doneTotal}}</span> / 全部{{total}}
+    </span>
+    <button class="btn btn-danger" @click="clearDone">清除已完成任务</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'FooterCount',
+    props:[
+      'todos',
+      // 'checkAllTodo','clearAllTodo'
+    ],
+    computed:{
+      // 全部事项的统计
+      total(){
+        return this.todos.length
+      },
+      doneTotal(){
+        // 将统计结果返回
+        return this.todos.reduce((pre,todo)=>{
+          // 若current.done为true，pre就+1，否则+0
+          return pre + (todo.done ? 1 : 0)
+        },0)
+      },
+      isAll:{
+        get(){
+          return this.doneTotal===this.total && this.total>0
+        },
+        set(value){
+          // 发生变化就调用该函数，并把value作为参数传进去，有一个方便的优点就是，取消全选时，原先勾选上todoitem的会取消勾选，双向数据绑定嘛，这么写就不用配置methods了
+          // this.checkAllTodo(value)
+          this.$emit('checkAllTodo',value)
+        }
+      }
+    },
+    methods:{
+      clearDone(){
+        if(confirm('你来真的？')){
+          this.$emit('clearAllTodo')
+        }
+      }
+    }
+}
+</script>
+
+<style scoped>
+  /*footer*/
+  .todo-footer {
+    height: 40px;
+    line-height: 40px;
+    padding-left: 6px;
+    margin-top: 5px;
+  }
+
+  .todo-footer label {
+    display: inline-block;
+    margin-right: 20px;
+    cursor: pointer;
+  }
+
+  .todo-footer label input {
+    position: relative;
+    top: -1px;
+    vertical-align: middle;
+    margin-right: 5px;
+  }
+
+  .todo-footer button {
+    float: right;
+    margin-top: 5px;
+  }
+</style>
