a9adcfa9fa057600885ee6fd57db4523f40cb622
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2024-01-29 19:51:58 +0900

    accomplish TodoList case with GlobalEventBus

---
+++ 17-TodoList案例-全局事件总线实现/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul class="todo-main">
+    <TodoItem 
+      v-for="todoObj in todos" 
+      :key="todoObj.id" 
+      :todo="todoObj" 
+    />
+  </ul>
+</template>
+
+<script>
+  import TodoItem from './TodoItem'
+  export default {
+      name:'TodoList',
+      components:{TodoItem},
+      // checkTodo只是在这里走一趟，立马要去TodoItem组件
+      props:[
+        'todos',
+        // 'checkTodo','deleteTodo'
+      ]
+  }
+</script>
+
+<style scoped>
+  /*main*/
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
