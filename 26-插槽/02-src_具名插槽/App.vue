7fbb46c65fe404e0ad885426a6187b3a7a014522
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2024-02-05 10:05:08 +0900

    named slot

---
+++ 26-插槽/02-src_具名插槽/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <!-- 真正要展示地直接写在组件标签里了，就不需要通过:listData="food"这种方式传递数据了，同理下面的两个也要删除 -->
+    <EntertainmentCategory title="美食">
+      <!-- Vue把下面这一行解析成DOM后，不知道往哪儿放 -->
+      <!-- img标签往名为center的插槽里放 -->
+      <img slot="center" src="https://img1.baidu.com/it/u=4097841643,3109051827&fm=253&fmt=auto&app=138&f=JPEG?w=667&h=500" alt="">
+      <!-- a标签往名为Footer的插槽里放 -->
+      <a slot="footer" href="http://www.baidu.com">更多美食</a>
+    </EntertainmentCategory>
+    <EntertainmentCategory title="游戏">
+      <ul slot="center">
+        <!-- 在数据所在组件遍历数据时，就不用listData了，直接在游戏的组件标签里遍历游戏数据 -->
+        <li v-for="(game,index) in games" :key="index">{{game}}</li>
+      </ul>
+      <!-- 写多个也不会覆盖 -->
+      <div slot="footer" class="foot">
+        <a href="http://www.baidu.com">单机游戏</a>
+        <a href="http://www.baidu.com">网络游戏</a>
+      </div>
+    </EntertainmentCategory>
+    <EntertainmentCategory title="电影">
+      <video slot="center" controls src="http://www.baidu.com/link?url=oFgYRA8d0VKF7hKi-oWuKbx-PjgmTrPb7CUN15Qn-ddpdvviGR1D6GHCrekpKWAvXEcGkEURjAr-llqy5p-dGq"></video>
+      <!-- v-slot属性只能用在组件的template标签上 -->
+      <template v-slot:footer>
+        <div class="foot">
+          <a href="http://www.baidu.com">经典</a>
+          <a href="http://www.baidu.com">热门</a>
+          <a href="http://www.baidu.com">推荐</a>
+        </div>
+        <h4 class="foot">欢迎影迷前来观看</h4>
+      </template>
+    </EntertainmentCategory>
+  </div>
+</template>
+
+<script>
+  import EntertainmentCategory from './components/EntertainmentCategory'
+  export default {
+      name:'App',
+      components:{EntertainmentCategory},
+      data() {
+        return {
+          food:['火锅','烧烤','米线','锅包肉'],
+          games:['光与夜之恋','未定事件簿','恋与制作人','和平精英'],
+          films:['《封神第一部》','《复联4》','《黑豹》','《红海行动》']
+        }
+      },
+  }
+</script>
+
+<style scoped>
+  .container,.foot{
+    display: flex;
+    justify-content: space-around;
+  }
+  img{
+    width:100%
+  }
+  video{
+    width: 100%;
+  }
+  h4{
+    text-align: center;
+  }
+</style>
---
+++ 26-插槽/02-src_具名插槽/components/EntertainmentCategory.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="category">
+    <h3>{{title}}分类</h3>
+    <!-- 打标识，也是占位符，准确一点是定义一个插槽，示意组件里的标签体放在这里，别瞎晃哒不知道去哪儿，也相当于挖个坑，等着组件的使用者进行填充 -->
+    <slot name="center">It is default value. It will appear, when user does not send a specific structure.1</slot>
+    <slot name="footer">It is default value. It will appear, when user does not send a specific structure.2</slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name:'EntertainmentCategory',
+        props:[
+            // 'listData',
+            'title'
+        ]
+    }
+</script>
+
+<style scoped>
+    .category{
+        background-color: lightpink;
+        width:200px;
+        height:300px;
+    }
+    h3{
+        text-align: center;
+        background-color: aqua;
+    }
+    
+</style>
