a194b8b5d05d1b64a6a2129fe943d96b8d49b824
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2024-02-07 21:40:03 +0900

    increment case with methods -- vue version

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,17 @@
 <template>
   <div>
-    <button @click="getStudentInfo">get student information</button>
-    <button @click="getCarInfo">get car information</button>
-
+    <NumberCount/>
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
+  import NumberCount from './components/NumberCount.vue'
   export default {
-      name:'App',
-      methods:{
-        getStudentInfo(){
-          // 找端口号为8080的代理服务器要数据，前缀要紧跟端口号，后面该怎么写怎么写
-          axios.get('http://localhost:8080/api/students').then(
-            response=>{
-              console.log('request succeed',response.data);
-            },
-            error=>{
-              console.warn('failed to request',error.message);
-            }
-          )
-        },
-        getCarInfo(){
-          axios.get('http://localhost:8080/demo/cars').then(
-            response=>{
-              console.log('request succeed',response.data);
-            },
-            error=>{
-              console.log('failed to request',error.message);
-            }
-          )
-        }
-      }
+    name:'App',
+    components:{NumberCount}
   }
 </script>
 
+<style>
+  
+</style>
