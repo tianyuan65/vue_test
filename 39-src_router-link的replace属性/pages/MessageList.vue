1d24015dcb6d0462534851701f3f65f72fbc88c7
goodMorning_pro@xxx.com <tianyuan990605@gmail.com>
2024-02-18 19:35:44 +0900

    replace property of router-link

---
+++ 39-src_router-link的replace属性/pages/MessageList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <ul>
+        <li v-for="m in messageItem" :key="m.id">
+            <!-- 跳转路由并携带params参数，to的字符串写法，与query不同，直接在detail后面加/，并用${} -->
+            <!-- <router-link :to="`/home/message/detail/${m.id}/${m.title}`">{{m.title}}</router-link>&nbsp;&nbsp; -->
+            <router-link to="/home/message/detail?id=${m.id}&title=${m.title}"></router-link>
+
+            <!-- 跳转路由并携带params参数，to的对象写法，使用params参数不允许使用path配置，只能使用name配置 -->
+            <router-link :to="{
+                name:'xiangqing',
+                query:{
+                    id:m.id,
+                    title:m.title
+                }
+            }">
+                {{m.title}}
+            </router-link>&nbsp;&nbsp;
+        </li>
+    </ul>
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+    export default {
+        name:'MessageList',
+        data(){
+            return {
+                messageItem:[
+                    {id:'001',title:'msg001'},
+                    {id:'002',title:'msg002'},
+                    {id:'003',title:'msg003'}
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- README.md
+++ README.md
@@ -850,6 +850,10 @@
             // },
         }
       ```
+* 10. <router-link>的replace属性
+    * 10.1 作用：控制路由跳转时操作浏览器历史记录的模式
+    * 10.2 浏览器的历史记录有两种写入方式：分别是push和replace，push是追加历史记录，replace是替换当前记录。路由跳转的时候默认是push
+    * 10.3 如何开启replace模式：```<router-link replace class="list-group-item" active-class="active" to="/about">About</router-link>```
 
 
 
